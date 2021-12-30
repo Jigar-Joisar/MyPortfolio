c99ca50aa8834b5cc2966e8e3c1ce9d5ffcc2541
JIGAR JOISAR <jjoisar2221@gmail.com>
2021-12-30 21:41:16 +0530

    added gif to background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import backgroundH from './background.gif';
 
+const styles = {
+  
+      backgroundImage: `url(${backgroundH})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      width: '100vw',
+      height: '100vh'
+  
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  style={styles}>
+        <div className='App'>
+          JIGAR JOISAR
+        </div>
     </div>
+    
   );
 }
 
